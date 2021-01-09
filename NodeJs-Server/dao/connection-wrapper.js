e8e97dd9f9b4c833716e42787524cc723a6389be
Ariel Cohen <33195669+MrARx2@users.noreply.github.com>
2021-01-09 22:21:23 +0200

    Add files via upload

--- NodeJs-Server/dao/connection-wrapper.js
+++ NodeJs-Server/dao/connection-wrapper.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host: "localhost", // Computer
     user: "root", // Username
-    password: "Aa857357451", // Password
+    password: "1234", // Password
     database: "vacation_lover" // Database name
 });
 
